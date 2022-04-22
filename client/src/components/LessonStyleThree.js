8e1ec27d440609812e36d56387b63d7d0d3d5b5c
thumminbird14 <tanyapurcell@hotmail.co.uk>
2022-04-22 15:50:09 +0100

    add sentences

--- client/src/components/LessonStyleThree.js
+++ client/src/components/LessonStyleThree.js
@@ -1,3 +1,22 @@
 
 
-const LessonStyleThree
+const LessonStyleThree = () => {
+
+    return (
+        <>
+        <h2>Animals Lesson</h2>
+        <p>The dog chases a ball</p>
+        <p>The cat is asleep</p>
+        <p>The raccoon is eating fish</p>
+        <p>The duck swims on the pond</p>
+        <p>The eagle flies high</p>
+        <p>The dinosaur is frightening</p>
+        <p>The pig lives on the farm</p>
+        <p>The frog goes "Croak"</p>
+        </>
+
+    );
+
+}
+
+export default LessonStyleThree;
