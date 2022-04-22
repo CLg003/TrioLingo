9990d50566a7a16e08c4256f2a3b8ea90ea72b76
CLg003 <clairemjlaing@gmail.com>
2022-04-22 14:18:03 +0100

    Words data extended and ReadMe updated.

--- client/src/words_data.js
+++ client/src/words_data.js
@@ -1,42 +1,121 @@
 
 export const words_data = {
-    // topic: {
-        animals: {
-            dog: {
-                translation: "perro",
-                img: ""
-            },
-            cat: "gato",
-            racoon: "mapache",
-            duck: "pato",
-            eagle: "agila",
-            dinosaur: "dinosaurio",
-            pig: "cerdo",
-            frog: "rana"
+    animals: {
+        dog: {
+            translation: "perro",
+            img: ""
         },
-        colours: {
-            red: "rojo",
-            green: "verde",
-            yellow: "amarillo",
-            orange: "naranja",
-            brown: "marron",
-            blue: "azul",
-            white: "blanco",
-            black: "negro",
-            purple: "morado",
+        cat: {
+            translation: "gato",
+            img: ""
         },
-        numbers: {
-            zero: "cero",
-            one: "uno",
-            two: "dos",
-            three: "tres",
-            four: "cuatro",
-            five: "cinco",
-            six: "seis",
-            seven:"siete",
-            eight: "ocho",
-            nine: "nueve",
-            ten: "diez"
+        racoon: {
+            translation: "mapache",
+            img: ""
         },
-    // }
-  }
+        duck: {
+            translation: "pato",
+            img: ""
+        },
+        eagle: {
+            translation: "agila",
+            img: ""
+        },
+        dinosaur: {
+            translation: "dinosaurio",
+            img: ""
+        },
+        pig: {
+            translation: "cerdo",
+            img: ""
+        },
+        frog: {
+            translation: "rana",
+            img: ""
+        }
+    },
+    colours: {
+        red: {
+            translation: "rojo",
+            img: ""
+        },
+        green: {
+            translation: "verde",
+            img: ""
+        },
+        yellow: {
+            translation: "amarillo",
+            img: ""
+        },
+        orange: {
+            translation: "naranja",
+            img: ""
+        },
+        brown: {
+            translation: "marron",
+            img: ""
+        },
+        blue: {
+            translation: "azul",
+            img: ""
+        },
+        white: {
+            translation: "blanco",
+            img: ""
+        },
+        black: {
+            translation: "negro",
+            img: ""
+        },
+        purple: {
+            translation: "morado",
+            img: ""
+        },
+    },
+    numbers: {
+        zero: {
+            translation: "cero",
+            img: ""
+        },
+        one: {
+            translation: "uno",
+            img: ""
+        },
+        two: {
+            translation: "dos",
+            img: ""
+        },
+        three: {
+            translation: "tres",
+            img: ""
+        },
+        four: {
+            translation: "cuatro",
+            img: ""
+        },
+        five: {
+            translation: "cinco",
+            img: ""
+        },
+        six: {
+            translation: "seis",
+            img: ""
+        },
+        seven: {
+            translation: "siete",
+            img: ""
+        },
+        eight: {
+            translation: "ocho",
+            img: ""
+        },
+        nine: {
+            translation: "nueve",
+            img: ""
+        },
+        ten: {
+            translation: "diez",
+            img: ""
+        }
+    },
+}
