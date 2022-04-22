0c01cd918cd06d3c779069b35dcb721e635af5ee
CordiiT14 <cordelia.toennies@live.com>
2022-04-22 17:52:09 +0100

    component now displays numbers, conditional logic to flip between english and spanish, button onclick not yet implemented

--- client/src/components/LessonStyleOne.js
+++ client/src/components/LessonStyleOne.js
@@ -1,6 +1,74 @@
+import {useState} from 'react';
+import './lessonstyleone.css';
 
-const LessonStyleOne = () => {
+const LessonStyleOne = ({words}) => {
+
+    const [translate, setTranslate ] = useState(false)
+
+    return(
+        <div>
+        <ul>
+            <li>
+                <div>
+                    {translate ? <h3>{words.zero.translation}</h3> : <h3>Zero</h3> }
+                </div>
+            </li>
+            <li>
+                <div>
+                    {translate ? <h3>{words.one.translation}</h3> : <h3>One</h3>}
+                </div>
+            </li>
+            <li>
+                <div>
+                    {translate ? <h3>{words.two.translation}</h3> : <h3>Two</h3>}
+                </div>
+            
+            </li>
+            <li>
+                <div>
+                    {translate ? <h3>{words.three.translation}</h3> : <h3>Three</h3>}
+                </div>
+            </li>
+            <li>
+                <div>
+                    {translate ? <h3>{words.four.translation}</h3> : <h3>Four</h3>}
+                </div>
+            </li>
+            <li>
+                <div>
+                    {translate ? <h3>{words.five.translation}</h3> : <h3>Five</h3>}
+                </div>
+            </li>
+            <li>
+                <div>
+                    {translate ? <h3>{words.six.translation}</h3> : <h3>Six</h3>}
+                </div>
+            </li>
+            <li>
+                <div>
+                    {translate ? <h3>{words.seven.translation}</h3> : <h3>Seven</h3>}
+                </div>
+            </li>
+            <li>
+                <div>
+                    {translate ? <h3>{words.eight.translation}</h3> : <h3>Eight</h3>}
+                </div>
+            </li>
+            <li>
+                <div>
+                    {translate ? <h3>{words.nine.translation}</h3> : <h3>Nine</h3>}
+                </div>
+            </li>
+
+        </ul>
+        <div>
+            <button>
+                Translate
+            </button>
+        </div>
+        </div>
+    )
     
-}
+};
 
 export default LessonStyleOne;
