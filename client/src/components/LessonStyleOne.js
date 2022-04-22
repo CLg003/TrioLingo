c6c094904fb5069e4b099810ff10dbd919d7c778
CordiiT14 <cordelia.toennies@live.com>
2022-04-22 20:27:44 +0100

    animation added to elements, triggered by button click

--- client/src/components/LessonStyleOne.js
+++ client/src/components/LessonStyleOne.js
@@ -8,15 +8,18 @@ const LessonStyleOne = ({words}) => {
 
     const toggleState = () => {
         setIsActive(!isActive);
-        // setTranslate(!translate);
+       
     };
 
+    const translateWord = () => {
+         setTranslate(!translate);
+    }
 
     return(
         <div >
         <ul className='numbers'>
             <li>
-                <div className={ isActive? 'number-item' : 'zero'} >
+                <div className={ isActive? 'number-item' : 'zero'} onAnimationStart={translateWord}>
                     {translate ? <h3>{words.zero.translation}</h3> : <h3>zero</h3> }
                 </div>
             </li>
@@ -26,43 +29,43 @@ const LessonStyleOne = ({words}) => {
                 </div>
             </li>
             <li>
-                <div className={ isActive? 'number-item' : 'two'} onAnimationEnd={toggleState}>
+                <div className={ isActive? 'number-item' : 'two'} >
                     {translate ? <h3>{words.two.translation}</h3> : <h3>two</h3>}
                 </div>
             
             </li>
             <li>
-                <div className='number-item'>
+                <div className={ isActive? 'number-item' : 'three'}>
                     {translate ? <h3>{words.three.translation}</h3> : <h3>three</h3>}
                 </div>
             </li>
             <li>
-                <div className='number-item'>
+                <div className={ isActive? 'number-item' : 'four'}>
                     {translate ? <h3>{words.four.translation}</h3> : <h3>four</h3>}
                 </div>
             </li>
             <li>
-                <div className='number-item'>
+                <div className={ isActive? 'number-item' : 'five'}>
                     {translate ? <h3>{words.five.translation}</h3> : <h3>five</h3>}
                 </div>
             </li>
             <li>
-                <div className='number-item'>
+                <div className={ isActive? 'number-item' : 'six'}>
                     {translate ? <h3>{words.six.translation}</h3> : <h3>six</h3>}
                 </div>
             </li>
             <li>
-                <div className='number-item'>
+                <div className={ isActive? 'number-item' : 'seven'}>
                     {translate ? <h3>{words.seven.translation}</h3> : <h3>seven</h3>}
                 </div>
             </li>
             <li>
-                <div className='number-item'>
+                <div className={ isActive? 'number-item' : 'eight'}>
                     {translate ? <h3>{words.eight.translation}</h3> : <h3>eight</h3>}
                 </div>
             </li>
             <li>
-                <div className='number-item'>
+                <div className={ isActive? 'number-item' : 'nine'} onAnimationEnd={toggleState && translateWord}>
                     {translate ? <h3>{words.nine.translation}</h3> : <h3>nine</h3>}
                 </div>
             </li>
