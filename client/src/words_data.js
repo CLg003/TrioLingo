27d4a2e3217116d7b017cb8f8e9947fc50d7e322
elicodes89 <elicodes89@gmail.com>
2022-04-23 12:39:51 +0100

    changed words data and added english

--- client/src/words_data.js
+++ client/src/words_data.js
@@ -3,120 +3,149 @@
 export const words_data = {
     animals: {
         dog: {
+            english: "dog",
             translation: "perro",
             img: "src/images/animals/icons8-fat-dog-96.png"
         },
         cat: {
+            english: "cat",
             translation: "gato",
             img: "src/images/animals/icons8-cat-96.png"
         },
 
-        fish: { /// racoon not available
+        fish: { 
+            english: "fish",
             translation: "pescado",
             img: "src/images/animals/icons8-flounder-fish-96.png"
         },
         duck: {
+            english: "duck",
             translation: "pato",
             img: "src/images/animals/icons8-rubber-duck-96.png"
         },
 
-        penguin: { // eagle not available
+        penguin: { 
+            english: "penguin",
             translation: "pinguino",
             img: "src/images/animals/icons8-penguin-96.png"
         },
         dinosaur: {
+            english: "dinosaur",
             translation: "dinosaurio",
             img: "src/images/animals/icons8-kawaii-dinosaur-96.png"
         },
         pig: {
+            english: "pig",
             translation: "cerdo",
             img: "src/images/animals/icons8-money-box-96.png"
         },
         frog: {
+            english: "frog",
             translation: "rana",
             img: "src/images/animals/icons8-frog-96.png"
         }
     },
     colours: {
         red: {
+            english: "red",
             translation: "rojo",
             img: "src/images/colours/icons8-fill-color-96RED.png"
         },
         green: {
+            english: "green",
             translation: "verde",
             img: "src/images/colours/icons8-fill-color-96GREEN.png"
         },
         yellow: {
+            english: "yellow",
             translation: "amarillo",
             img: "src/images/colours/icons8-fill-color-96YELLOW.png"
         },
         orange: {
+            english: "orange",
             translation: "naranja",
             img: "src/images/colours/icons8-fill-color-96ORANGE.png"
         },
         brown: {
+            english: "brown",
             translation: "marron",
             img: "src/images/colours/icons8-fill-color-96BROWN.png"
         },
         blue: {
+            english: "blue",
             translation: "azul",
             img: "src/images/colours/icons8-fill-color-96BLUE.png"
         },
         white: {
+            english: "white",
             translation: "blanco",
             img: "src/images/colours/icons8-fill-color-96WHITE.png"
         },
         black: {
+            english: "black",
             translation: "negro",
             img: "src/images/colours/icons8-fill-color-96BLACK.png"
         },
         purple: {
+            english: "purple",
             translation: "morado",
             img: "src/images/colours/icons8-fill-color-96PURPLE.png"
         },
     },
     numbers: {
         zero: {
+
+            english: "zero",
             translation: "cero",
             img: "src/images/numbers/0.png"
         },
         one: {
+            english: "one",
             translation: "uno",
             img: "src/images/numbers/001-1.png"
         },
-        two: {
+        two: {            
+            english: "two",
             translation: "dos",
             img: "src/images/numbers/002-2.png"
         },
         three: {
+            english: "three",
             translation: "tres",
             img: "src/images/numbers/003-3.png"
         },
         four: {
+            english: "four",
             translation: "cuatro",
             img: "src/images/numbers/004-4.png"
         },
         five: {
+            english: "five",
             translation: "cinco",
             img: "src/images/numbers/005-5.png"
         },
         six: {
+            english: "six",
             translation: "seis",
             img: "src/images/numbers/006-6.png"
         },
         seven: {
+            english: "seven",
             translation: "siete",
             img: "src/images/numbers/007-7.png"
         },
         eight: {
+            english: "eight",
             translation: "ocho",
             img: "src/images/numbers/008-8.png"
         },
         nine: {
+            english: "nine",
             translation: "nueve",
             img: "src/images/numbers/009-9.png"
         },
         ten: {
+            english: "ten",
             translation: "diez",
             img: ""
         }
