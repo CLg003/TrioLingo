02c04179a81470a65e3024934dda2d781321ea13
CordiiT14 <cordelia.toennies@live.com>
2022-04-23 09:23:02 +0100

    added relative file paths

--- client/src/words_data.js
+++ client/src/words_data.js
@@ -4,119 +4,119 @@ export const words_data = {
     animals: {
         dog: {
             translation: "perro",
-            img: ""
+            img: "src/images/animals/icons8-fat-dog-96.png"
         },
         cat: {
             translation: "gato",
-            img: ""
+            img: "src/images/animals/icons8-cat-96.png"
         },
         fish: { /// racoon not available
-            translation: "",
+            translation: "src/images/animals/icons8-flounder-fish-96.png",
             img: ""
         },
         duck: {
             translation: "pato",
-            img: ""
+            img: "src/images/animals/icons8-rubber-duck-96.png"
         },
         penguin: { // eagle not available
             translation: "",
-            img: ""
+            img: "src/images/animals/icons8-penguin-96.png"
         },
         dinosaur: {
             translation: "dinosaurio",
-            img: ""
+            img: "src/images/animals/icons8-kawaii-dinosaur-96.png"
         },
         pig: {
             translation: "cerdo",
-            img: ""
+            img: "src/images/animals/icons8-money-box-96.png"
         },
         frog: {
             translation: "rana",
-            img: ""
+            img: "src/images/animals/icons8-frog-96.png"
         }
     },
     colours: {
         red: {
             translation: "rojo",
-            img: ""
+            img: "src/images/colours/icons8-fill-color-96RED.png"
         },
         green: {
             translation: "verde",
-            img: ""
+            img: "src/images/colours/icons8-fill-color-96GREEN.png"
         },
         yellow: {
             translation: "amarillo",
-            img: ""
+            img: "src/images/colours/icons8-fill-color-96YELLOW.png"
         },
         orange: {
             translation: "naranja",
-            img: ""
+            img: "src/images/colours/icons8-fill-color-96ORANGE.png"
         },
         brown: {
             translation: "marron",
-            img: ""
+            img: "src/images/colours/icons8-fill-color-96BROWN.png"
         },
         blue: {
             translation: "azul",
-            img: ""
+            img: "src/images/colours/icons8-fill-color-96BLUE.png"
         },
         white: {
             translation: "blanco",
-            img: ""
+            img: "src/images/colours/icons8-fill-color-96WHITE.png"
         },
         black: {
             translation: "negro",
-            img: ""
+            img: "src/images/colours/icons8-fill-color-96BLACK.png"
         },
         purple: {
             translation: "morado",
-            img: ""
+            img: "src/images/colours/icons8-fill-color-96PURPLE.png"
         },
     },
     numbers: {
         zero: {
             translation: "cero",
-            img: "https://cdn-icons-png.flaticon.com/512/3570/3570095.png"
+            img: "src/images/numbers/0.png"
         },
         one: {
             translation: "uno",
-            img: "https://cdn-icons-png.flaticon.com/512/3570/3570098.png"
+            img: "src/images/numbers/001-1.png"
         },
         two: {
             translation: "dos",
-            img: "https://cdn-icons-png.flaticon.com/512/3570/3570100.png"
+            img: "src/images/numbers/002-2.png"
         },
         three: {
             translation: "tres",
-            img: "https://cdn-icons-png.flaticon.com/512/3570/3570103.png"
+            img: "src/images/numbers/003-3.png"
         },
         four: {
             translation: "cuatro",
-            img: "https://cdn-icons-png.flaticon.com/512/3570/3570104.png"
+            img: "src/images/numbers/004-4.png"
         },
         five: {
             translation: "cinco",
-            img: "https://cdn-icons-png.flaticon.com/512/3570/3570107.png"
+            img: "src/images/numbers/005-5.png"
         },
         six: {
             translation: "seis",
-            img: "https://cdn-icons-png.flaticon.com/512/3570/3570112.png"
+            img: "src/images/numbers/006-6.png"
         },
         seven: {
             translation: "siete",
-            img: "https://cdn-icons-png.flaticon.com/512/3570/3570115.png"
+            img: "src/images/numbers/007-7.png"
         },
         eight: {
             translation: "ocho",
-            img: "https://cdn-icons-png.flaticon.com/512/3570/3570118.png"
+            img: "src/images/numbers/008-8.png"
         },
         nine: {
             translation: "nueve",
-            img: "https://cdn-icons-png.flaticon.com/512/3570/3570120.png"
+            img: "src/images/numbers/009-9.png"
         },
         ten: {
             translation: "diez",
-            img: "https://cdn-icons-png.flaticon.com/512/3570/3570098.png"
+            img: ""
         }
     },
 }
