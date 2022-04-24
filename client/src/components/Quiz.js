bd399ed6e4e1341aa96da4d08ace1ddcd6848c3b
CLg003 <clairemjlaing@gmail.com>
2022-04-24 13:40:30 +0100

    words_data.js image paths updated to assets folder.

--- client/src/components/Quiz.js
+++ client/src/components/Quiz.js
@@ -8,6 +8,8 @@ const Quiz = ({wordList}) => {
     const questionOneWord = copyWordList[Math.floor(Math.random()*copyWordList.length)];
     console.log(questionOneWord);
     copyWordList.splice((copyWordList.indexOf(questionOneWord)), 1);
+    const questionOneWrongAnswersList = wordList.splice((wordList.indexOf(questionOneWord)), 1);
+
 
     const questionTwoWord = copyWordList[Math.floor(Math.random()*copyWordList.length)];
     console.log(questionTwoWord);
@@ -24,6 +26,9 @@ const Quiz = ({wordList}) => {
     const questionFiveWord = copyWordList[Math.floor(Math.random()*copyWordList.length)];
     console.log(questionFiveWord);
     copyWordList.splice((copyWordList.indexOf(questionFiveWord)), 1);
+    
+    questionOneWrongAnswersList[Math.floor(Math.random()*questionOneWrongAnswersList.length)]
+
 
     return (
         <div id="quiz">
@@ -34,12 +39,15 @@ const Quiz = ({wordList}) => {
                     <img src={`${process.env.PUBLIC_URL}${questionOneWord.img}`}/>
                     <div className="question-text">
                         <p className="question-number">Question 1</p>
-                        <p className="question">What is the Spanish word for {}?</p>
+                        <p className="question">What is the Spanish word for {questionOneWord.english}?</p>
+
                         <label htmlFor="option-1">Option 1 </label>
                         <input type="radio" id="option-1" name="question-1" value="Option 1"/>
-                        <label>Option 2 </label>
+                        
+                        <label htmlFor="option-2">Option 2 </label>
                         <input type="radio" id="option-2" name="question-1" value="Option 2"/>
-                        <label>Option 3 </label>
+                        
+                        <label htmlFor="option-3">Option 3 </label>
                         <input type="radio" id="option-3" name="question-1" value="Option 3"/>
                     </div>
                 </div>
@@ -47,12 +55,15 @@ const Quiz = ({wordList}) => {
                     <img src={`${process.env.PUBLIC_URL}${questionTwoWord.img}`}/>
                     <div className="question-text">
                         <p className="question-number">Question 2</p>
-                        <p className="question">What is the English word for {}?</p>
+                        <p className="question">What is the English word for {questionTwoWord.translation}?</p>
+                        
                         <label htmlFor="option-1">Option 1 </label>
                         <input type="radio" id="option-1" name="question-2" value="Option 1"/>
-                        <label>Option 2 </label>
+                        
+                        <label htmlFor="option-2">Option 2 </label>
                         <input type="radio" id="option-2" name="question-2" value="Option 2"/>
-                        <label>Option 3 </label>
+                        
+                        <label htmlFor="option-3">Option 3 </label>
                         <input type="radio" id="option-3" name="question-2" value="Option 3"/>
                     </div>
                 </div>
@@ -60,12 +71,15 @@ const Quiz = ({wordList}) => {
                     <img src={`${process.env.PUBLIC_URL}${questionThreeWord.img}`}/>
                     <div className="question-text">
                         <p className="question-number">Question 3</p>
-                        <p className="question">What is the Spanish word for {}?</p>
+                        <p className="question">What is the Spanish word for {questionThreeWord.english}?</p>
+                        
                         <label htmlFor="option-1">Option 1 </label>
                         <input type="radio" id="option-1" name="question-3" value="Option 1"/>
-                        <label>Option 2 </label>
+                        
+                        <label htmlFor="option-2">Option 2 </label>
                         <input type="radio" id="option-2" name="question-3" value="Option 2"/>
-                        <label>Option 3 </label>
+                        
+                        <label htmlFor="option-3">Option 3 </label>
                         <input type="radio" id="option-3" name="question-3" value="Option 3"/>
                     </div>
                 </div>
@@ -73,12 +87,15 @@ const Quiz = ({wordList}) => {
                     <img src={`${process.env.PUBLIC_URL}${questionFourWord.img}`}/>
                     <div className="question-text">
                         <p className="question-number">Question 4</p>
-                        <p className="question">What is the English word for {}?</p>
+                        <p className="question">What is the English word for {questionFourWord.translation}?</p>
+                        
                         <label htmlFor="option-1">Option 1 </label>
                         <input type="radio" id="option-1" name="question-4" value="Option 1"/>
-                        <label>Option 2 </label>
+                        
+                        <label htmlFor="option-2">Option 2 </label>
                         <input type="radio" id="option-2" name="question-4" value="Option 2"/>
-                        <label>Option 3 </label>
+                        
+                        <label htmlFor="option-3">Option 3 </label>
                         <input type="radio" id="option-3" name="question-4" value="Option 3"/>
                     </div>
                 </div>
@@ -86,12 +103,15 @@ const Quiz = ({wordList}) => {
                     <img src={`${process.env.PUBLIC_URL}${questionFiveWord.img}`}/>
                     <div className="question-text">
                         <p className="question-number">Question 5</p>
-                        <p className="question">What is the Spanish word for {}?</p>
+                        <p className="question">What is the Spanish word for {questionFiveWord.english}?</p>
+                        
                         <label htmlFor="option-1">Option 1 </label>
                         <input type="radio" id="option-1" name="question-5" value="Option 1"/>
-                        <label>Option 2 </label>
+                        
+                        <label htmlFor="option-2">Option 2 </label>
                         <input type="radio" id="option-2" name="question-5" value="Option 2"/>
-                        <label>Option 3 </label>
+                        
+                        <label htmlFor="option-3">Option 3 </label>
                         <input type="radio" id="option-3" name="question-5" value="Option 3"/>
                     </div>
                 </div>
--- client/src/words_data.js
+++ client/src/words_data.js
@@ -5,44 +5,42 @@ export const words_data = {
         dog: {
             english: "dog",
             translation: "perro",
-            img: "../src/images/animals/icons8-fat-dog-96.png"
+            img: "/assets/images/animals/icons8-fat-dog-96.png"
         },
         cat: {
             english: "cat",
             translation: "gato",
-            img: "src/images/animals/icons8-cat-96.png"
+            img: "/assets/images/animals/icons8-cat-96.png"
         },
-
         fish: { 
             english: "fish",
             translation: "pescado",
-            img: "src/images/animals/icons8-flounder-fish-96.png"
+            img: "/assets/images/animals/icons8-flounder-fish-96.png"
         },
         duck: {
             english: "duck",
             translation: "pato",
-            img: "src/images/animals/icons8-rubber-duck-96.png"
+            img: "/assets/images/animals/icons8-rubber-duck-96.png"
         },
-
         penguin: { 
             english: "penguin",
             translation: "pinguino",
-            img: "src/images/animals/icons8-penguin-96.png"
+            img: "/assets/images/animals/icons8-penguin-96.png"
         },
         dinosaur: {
             english: "dinosaur",
             translation: "dinosaurio",
-            img: "src/images/animals/icons8-kawaii-dinosaur-96.png"
+            img: "/assets/images/animals/icons8-kawaii-dinosaur-96.png"
         },
         pig: {
             english: "pig",
             translation: "cerdo",
-            img: "src/images/animals/icons8-money-box-96.png"
+            img: "/assets/images/animals/icons8-money-box-96.png"
         },
         frog: {
             english: "frog",
             translation: "rana",
-            img: "src/images/animals/icons8-frog-96.png"
+            img: "/assets/images/animals/icons8-frog-96.png"
         }
     },
     colours: {
@@ -97,52 +95,52 @@ export const words_data = {
 
             english: "zero",
             translation: "cero",
-            img: "../images/numbers/0.png"
+            img: "/assets/images/numbers/0.png"
         },
         one: {
             english: "one",
             translation: "uno",
-            img: "src/images/numbers/001-1.png"
+            img: "/assets/images/numbers/001-1.png"
         },
         two: {            
             english: "two",
             translation: "dos",
-            img: "src/images/numbers/002-2.png"
+            img: "/assets/images/numbers/002-2.png"
         },
         three: {
             english: "three",
             translation: "tres",
-            img: "src/images/numbers/003-3.png"
+            img: "/assets/images/numbers/003-3.png"
         },
         four: {
             english: "four",
             translation: "cuatro",
-            img: "src/images/numbers/004-4.png"
+            img: "/assets/images/numbers/004-4.png"
         },
         five: {
             english: "five",
             translation: "cinco",
-            img: "src/images/numbers/005-5.png"
+            img: "/assets/images/numbers/005-5.png"
         },
         six: {
             english: "six",
             translation: "seis",
-            img: "src/images/numbers/006-6.png"
+            img: "/assets/images/numbers/006-6.png"
         },
         seven: {
             english: "seven",
             translation: "siete",
-            img: "src/images/numbers/007-7.png"
+            img: "/assets/images/numbers/007-7.png"
         },
         eight: {
             english: "eight",
             translation: "ocho",
-            img: "src/images/numbers/008-8.png"
+            img: "/assets/images/numbers/008-8.png"
         },
         nine: {
             english: "nine",
             translation: "nueve",
-            img: "src/images/numbers/009-9.png"
+            img: "/assets/images/numbers/009-9.png"
         },
         ten: {
             english: "ten",
