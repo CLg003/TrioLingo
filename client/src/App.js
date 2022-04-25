02caed5914f6acc0a41de4a83ad669e237168369
CLg003 <clairemjlaing@gmail.com>
2022-04-25 21:31:10 +0100

    Added basic home screen with user log in form.

--- client/src/App.js
+++ client/src/App.js
@@ -7,50 +7,76 @@ import LessonProgressBar from './components/LessonProgressBar';
 import {words_data} from './words_data.js';
 import LessonProgressionContext from './context/LessonProgressionContext';
 import {getWords} from './services/WordsService';
-import {getUsers} from './services/UsersService';
+import {getUsers, postUser} from './services/UsersService';
+import HomeScreen from './components/HomeScreen';
+
 
 function App() {
-  const [words, setWords] = useState(null);
-  const [users, setUsers] = useState([]);
-  const [lessonsCompleted, setLessonsCompleted] = useState(0);
+    const [words, setWords] = useState(null);
+    const [users, setUsers] = useState([]);
+    const [lessonsCompleted, setLessonsCompleted] = useState(0);
+    const [loggedInUser, setLoggedInUser] = useState(null);
 
-  useEffect(()=> {
-    getWords()
-    .then((data) => {
-      setWords(data);
-    })
-  },[]);
+    useEffect(()=> {
+        getWords()
+        .then((data) => {
+            setWords(data);
+        })
+    },[]);
 
-  useEffect(()=> {
-    getUsers()
-    .then((data) => {
-      setUsers(data);
-    })
-  },[]);
+    useEffect(()=> {
+        getUsers()
+        .then((data) => {
+            setUsers(data);
+        })
+    },[]);
 
-  
+    useEffect(() => {
+      ( loggedInUser ? setLessonsCompleted(loggedInUser.lessons_completed) : setLessonsCompleted(0) )
+    }, [loggedInUser])
 
-  // const wordsArray = words.map(word => {
-  //   return word;
-  // })
+    const logInUser = (nameInput) => {
+        const userToLogIn = users.filter(user => {
+            return user.name === nameInput;
+        })
+        if (userToLogIn) {
+            setLoggedInUser(userToLogIn);
+        } else { 
+            const newUser = {name : nameInput, lessons_completed: 0};
+            addUser(newUser);
+            setLoggedInUser(userToLogIn);
+        }
+    }
 
-  const lessonProgression = () => {
-    setLessonsCompleted(lessonsCompleted + 1);
-  }
+    const addUser = (newUser) => {
+      postUser(newUser)
+      .then((newUser) => {
+        setUsers([...users, newUser])
+      });
+  };
+
+    const lessonProgression = () => {
+        setLessonsCompleted(lessonsCompleted + 1);
+    }
 
 
 
-  return (
-    <div className="App">
-      <h1>Trash Panda at your service</h1>
-      <LessonProgressionContext.Provider value={{lessonProgression, lessonsCompleted}}>
-        {lessonsCompleted < 3 ?
-        <LessonList words={words} lessonsCompleted={lessonsCompleted}/>
-        : <Game words={words}/> } 
-      </LessonProgressionContext.Provider>
-      <LessonProgressBar lessonsCompleted={lessonsCompleted}/>
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1>Trash Panda at your service</h1>
+            {!loggedInUser ?
+            <HomeScreen logInUser={logInUser}/>
+            : <>
+            <LessonProgressionContext.Provider value={{lessonProgression, lessonsCompleted}}>
+                {lessonsCompleted < 3 ?
+                <LessonList words={words} lessonsCompleted={lessonsCompleted}/>
+                : <Game words={words}/> } 
+            </LessonProgressionContext.Provider>
+            <LessonProgressBar lessonsCompleted={lessonsCompleted}/>
+            </>
+            }
+        </div>
+    );
 }
 
 export default App;
