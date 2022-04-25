bff2bc11c6e7cdbd3e9430b0a0d27a63135995ce
CordiiT14 <cordelia.toennies@live.com>
2022-04-25 09:45:27 +0100

    progress bar created, ready to add to develop branch

--- client/src/components/LessonProgressBar.js
+++ client/src/components/LessonProgressBar.js
@@ -7,7 +7,7 @@ const LessonProgressBar = ({lessonsCompleted}) => {
     return (
         <>
         <ProgressBar 
-            percent={25}
+            percent={75}
             hasStepZero={false}
             filledBackground="linear-gradient(to right, #992dd9 , #2bcf18)"
             width={800}
@@ -16,7 +16,6 @@ const LessonProgressBar = ({lessonsCompleted}) => {
             <Step>
                 {( {accomplished}) => (
                     <>
-                    <p>Lesson One</p>
                     <img
                     style={{ filter: `grayscale(${accomplished ? 0 : 80}%)` }}
                     width="60"
@@ -28,7 +27,6 @@ const LessonProgressBar = ({lessonsCompleted}) => {
             <Step>
                 {( {accomplished}) => (
                     <>
-                    <p>Lesson Two</p>
                     <img 
                     style={{ filter: `grayscale(${accomplished ? 0 : 80}%)` }}
                     width="60"
@@ -39,7 +37,6 @@ const LessonProgressBar = ({lessonsCompleted}) => {
             <Step>
                 {( {accomplished}) => (
                     <>
-                    <p>Lesson Three</p>
                     <img 
                     style={{ filter: `grayscale(${accomplished ? 0 : 80}%)` }}
                     width="60"
