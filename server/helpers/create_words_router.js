36f8c3d62f76541bbd5850fda227556c17ea4f73
thumminbird14 <tanyapurcell@hotmail.co.uk>
2022-04-25 13:40:12 +0100

    add remaining routes

--- server/helpers/create_words_router.js
+++ server/helpers/create_words_router.js
@@ -17,6 +17,31 @@ const createWordsRouter = function(collection) {
         });
     });
 
+    router.get('/:id', (req, res) => {
+        const id = req.params.id;
+        collection
+        .findOne({_id: ObjectID(id)})
+        .then((doc) => res.json(doc))
+        .catch((err) => {
+            console.error(err);
+            res.status(500);
+            res.json({status: 500, error: err});
+        });
+
+    });
+
+    router.delete('/:id' , (req, res) => {
+        const id = req.params.id;
+        collection
+        .deleteOne({ _id: ObjectID(id)})
+        .then(result => res.json(result))
+        .catch((err) => {
+          console.error(err);
+          res.status(500);
+          res.json({status: 500, error : err});
+        });
+      })
+
     return router;
 };
 module.exports = createWordsRouter;
