d807ab88286ccfbaaa215579024fe95dc0371b45
CordiiT14 <cordelia.toennies@live.com>
2022-04-25 08:02:08 +0100

    attempting refactor of quiz, because I couldn't stop thinking about it last night

--- client/src/components/Quiz.js
+++ client/src/components/Quiz.js
@@ -88,119 +88,69 @@ const handleSubmit = (event) => {
     console.log(event);
 }
 
+// Shuffle Function required for refactor
+
+const shuffle = (wordList) => {
+    let currentId = wordList.length;
+
+    while (currentId !== 0) {
+        let randomId = Math.floor(Math.random()* currentId);
+        currentId -= 1;
+
+        let temp = wordList[currentId];
+        wordList[currentId] = wordList[randomId];
+        wordList[randomId] = temp;
+    }
+    return wordList;
+}
+
+// Refactor of quiz logic
+
+const quizQuestions = () => {
+
+    let quizList = shuffle(wordList);
+
+    let wrongAnswers = [...quizList];
+    
+    for(let i = 0; i < 6 ; i++){
+        wrongAnswers = wrongAnswers.splice[i, 1];
+        wrongAnswers = shuffle(wrongAnswers);
+        let wrong1 = wrongAnswers[i]
+        let wrong2 = wrongAnswers[i++]
+    
+        return(
+            <div>
+                <img src={`${process.env.PUBLIC_URL}${quizList[i].img}`} alt="Quiz topic" />
+                <p className="question-number">Question {i+1}</p>
+                <p className="question">What is the Spanish word for {quizList[i].english}?</p>
+                
+                {/* WRONG ANSWER */}
+                <label htmlFor="option-1">{wrong1.translation}</label>
+                <input type="radio" id="option-1" name="question-1" value="wrong"/>
+                        
+                {/* WRONG ANSWER */}
+                <label htmlFor="option-2">{wrong2.translation}</label>
+                <input type="radio" id="option-2" name="question-1" value="wrong"/>
+                        
+                {/* CORRECT ANSWER */}
+                <label htmlFor="option-3">{quizList[i].translation}</label>
+                <input type="radio" id="option-3" name="question-1" value={questionOneWord.translation}/>
+            </div>
+        )
+    }
+
+}
+
+
     return (
         <div id="quiz">
             <h3 id="topic-heading">Quiz</h3>
             <p>Have fun with this little quiz to test your Spanish!</p>
             <form onSubmit={handleSubmit}>
 
-                {/* QUESTION 1 */}
-                <div className="question-div q1-div">
-                    <img src={`${process.env.PUBLIC_URL}${questionOneWord.img}`} alt="Quiz topic"/>
-                    <div className="question-text">
-                        <p className="question-number">Question 1</p>
-                        <p className="question">What is the Spanish word for {questionOneWord.english}?</p>
+            {quizQuestions()}
 
-                        {/* WRONG ANSWER */}
-                        <label htmlFor="option-1">{q1WrongA.translation}</label>
-                        <input type="radio" id="option-1" name="question-1" value="wrong"/>
-                        
-                        {/* WRONG ANSWER */}
-                        <label htmlFor="option-2">{q1WrongB.translation}</label>
-                        <input type="radio" id="option-2" name="question-1" value="wrong"/>
-                        
-                        {/* CORRECT ANSWER */}
-                        <label htmlFor="option-3">{questionOneWord.translation}</label>
-                        <input type="radio" id="option-3" name="question-1" value={questionOneWord.translation}/>
-                    </div>
-                </div>
-
-                {/* QUESTION 2 */}
-                <div className="question-div q2-div">
-                    <img src={`${process.env.PUBLIC_URL}${questionTwoWord.img}`} alt="Quiz topic"/>
-                    <div className="question-text">
-                        <p className="question-number">Question 2</p>
-                        <p className="question">What is the English word for {questionTwoWord.translation}?</p>
-                        
-                        {/* WRONG ANSWER */}
-                        <label htmlFor="option-1">{q2WrongA.english}</label>
-                        <input type="radio" id="option-1" name="question-2" value="wrong"/>
-                        
-                        {/* CORRECT ANSWER */}
-                        <label htmlFor="option-2">{questionTwoWord.english}</label>
-                        <input type="radio" id="option-2" name="question-2" value={questionTwoWord.english}/>
-                        
-                        {/* WRONG ANSWER */}
-                        <label htmlFor="option-3">{q2WrongB.english}</label>
-                        <input type="radio" id="option-3" name="question-2" value="wrong"/>
-                    </div>
-                </div>
-
-                {/* QUESTION 3 */}
-                <div className="question-div q3-div">
-                    <img src={`${process.env.PUBLIC_URL}${questionThreeWord.img}`} alt="Quiz topic"/>
-                    <div className="question-text">
-                        <p className="question-number">Question 3</p>
-                        <p className="question">What is the Spanish word for {questionThreeWord.english}?</p>
-                        
-                        {/* WRONG ANSWER */}
-                        <label htmlFor="option-1">{q3WrongA.translation}</label>
-                        <input type="radio" id="option-1" name="question-3" value="wrong"/>
-                        
-                        {/* WRONG ANSWER */}
-                        <label htmlFor="option-2">{q3WrongB.translation}</label>
-                        <input type="radio" id="option-2" name="question-3" value="wrong"/>
-                        
-                        {/* CORRECT ANSWER */}
-                        <label htmlFor="option-3">{questionThreeWord.translation} </label>
-                        <input type="radio" id="option-3" name="question-3" value={questionThreeWord.translation}/>
-                    </div>
-                </div>
-
-                {/* QUESTION 4 */}
-                <div className="question-div q4-div">
-                    <img src={`${process.env.PUBLIC_URL}${questionFourWord.img}`} alt="Quiz topic"/>
-                    <div className="question-text">
-                        <p className="question-number">Question 4</p>
-                        <p className="question">What is the English word for {questionFourWord.translation}?</p>
-                        
-                        {/* CORRECT ANSWER */}
-                        <label htmlFor="option-1">{questionFourWord.english} </label>
-                        <input type="radio" id="option-1" name="question-4" value={questionFourWord.english}/>
-                        
-                        {/* WRONG ANSWER */}
-                        <label htmlFor="option-2">{q4WrongA.english}</label>
-                        <input type="radio" id="option-2" name="question-4" value="wrong"/>
-                        
-                        {/* WRONG ANSWER */}
-                        <label htmlFor="option-3">
-                            {q4WrongB.english} 
-                        </label>
-                        <input type="radio" id="option-3" name="question-4" value="wrong"/>
-                    </div>
-                </div>
-
-                {/* QUESTION 5 */}
-                <div className="question-div q5-div">
-                    <img src={`${process.env.PUBLIC_URL}${questionFiveWord.img}`} alt="Quiz topic"/>
-                    <div className="question-text">
-                        <p className="question-number">Question 5</p>
-                        <p className="question">What is the Spanish word for {questionFiveWord.english}?</p>
-                        
-                        {/* WRONG ANSWER */}
-                        <label htmlFor="option-1">{q5WrongA.english}</label>
-                        <input type="radio" id="option-1" name="question-5" value="wrong"/>
-                        
-                        {/* CORRECT ANSWER */}
-                        <label htmlFor="option-2">{questionFiveWord.translation} </label>
-                        <input type="radio" id="option-2" name="question-5" value={questionFiveWord.translation}/>
-                        
-                        {/* WRONG ANSWER */}
-                        <label htmlFor="option-3">{q5WrongB.english}</label>
-                        <input type="radio" id="option-3" name="question-5" value="wrong"/>
-                    </div>
-                </div>
-                <input type="submit" >
+            <input type="submit" >
 
                 </input>
 
