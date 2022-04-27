d4b237dd8d013acefb0c2ea6a8fd95d87e030ecc
elicodes89 <elicodes89@gmail.com>
2022-04-27 10:13:53 +0100

    last game's modal timeout fixed. Set to 3secs

--- client/src/App.js
+++ client/src/App.js
@@ -14,8 +14,8 @@ function App() {
 
     const [words, setWords] = useState(null);
     const [users, setUsers] = useState([]);
-    const [lessonsCompleted, setLessonsCompleted] = useState(0);
-    const [loggedInUser, setLoggedInUser] = useState(null);
+    const [lessonsCompleted, setLessonsCompleted] = useState();
+    const [loggedInUser, setLoggedInUser] = useState();
 
 
     useEffect(()=> {
